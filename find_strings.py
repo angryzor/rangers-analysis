3342ae648b47fcc9d2936b91c6a49ee5abd1d961
angryzor <ruben_tytgat@hotmail.com>
2023-10-07 02:03:36 +0200

    Less lax strings

--- find_strings.py
+++ find_strings.py
@@ -11,7 +11,7 @@ cur_ea = seg.start_ea
 
 while cur_ea < seg.end_ea:
     if ida_bytes.is_unknown(ida_bytes.get_flags(cur_ea)):
-        strlen = ida_bytes.get_max_strlit_length(cur_ea, ida_nalt.STRTYPE_C, ida_bytes.ALOPT_IGNCLT | ida_bytes.ALOPT_IGNPRINT | ida_bytes.ALOPT_IGNHEADS)
+        strlen = ida_bytes.get_max_strlit_length(cur_ea, ida_nalt.STRTYPE_C, ida_bytes.ALOPT_IGNCLT | ida_bytes.ALOPT_IGNHEADS)
 
         if strlen > 5:
             ida_bytes.del_items(cur_ea, 0, strlen)
